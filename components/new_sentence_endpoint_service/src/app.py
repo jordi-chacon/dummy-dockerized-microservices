333bea8ebed9d6dd4a27e048d662b89483bfe876
Jordi Chacon <jordi.chacon@gmail.com>
2015-01-21 18:48:34 +0100

    Got the web UI to send requests to the python web server

--- components/new_sentence_endpoint_service/src/app.py
+++ components/new_sentence_endpoint_service/src/app.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import os
 import logging
 from logging.handlers import RotatingFileHandler
+
 app = Flask(__name__)
 
+def publish_to_mq(sender, message, language):
+    return
+
 @app.route('/sentences', methods=['POST'])
-def hello():
-    app.logger.info('Request received')
-    return 'BURRU'
+def new_sentence():
+    json=request.get_json()
+    sender=json["sender"]
+    message=json["sentence"]
+    language=json["language"]
+    publish_to_mq(sender, message, language)
+    return jsonify({}), 201
 
 if __name__ == "__main__":
     handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)
--- components/nginx/src/js/script.js
+++ components/nginx/src/js/script.js
@@ -1,24 +1,28 @@
-$("form#post_message_form").submit(function(e){
-  e.preventDefault();
-  var data = {}
-  var Form = this;
-  $.each(this.elements, function(i, v){
-    var input = $(v);
-    data[input.attr("name")] = input.val();
-    delete data["undefined"];
-  });
-  $.ajax({
-    cache: false,
-    url : "/post_message",
-    type: "POST",
-    dataType : "json",
-    data : JSON.stringify(data),
-    context : Form,
-    success : function(callback){
-      console.log(JSON.parse(callback));
-      $(this).html("Success!");
-    },
-    error : function(){
-      $(this).html("Error!");
-    }
-  });
+$( document ).ready(function() {
+    $("form#new_message_form").submit(function(e){
+	e.preventDefault();
+	var data = {}
+	var Form = this;
+	$.each(this.elements, function(i, v){
+	    var input = $(v);
+	    data[input.attr("name")] = input.val();
+	    delete data["undefined"];
+	});
+	$.ajax({
+	    cache: false,
+	    url : "/sentences",
+	    type: "POST",
+	    dataType : "json",
+	    contentType: "application/json",
+	    data : JSON.stringify(data),
+	    context : Form,
+	    success : function(callback){
+		console.log(JSON.parse(callback));
+		$(this).html("Success!");
+	    },
+	    error : function(){
+		$(this).html("Error!");
+	    }
+	});
+    });
+});
